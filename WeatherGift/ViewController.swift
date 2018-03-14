35ad6d05749675bd3afc17c091feb4f1c51c793f
Michael Green <MGreen@Michaels-MacBook-Air.local>
2018-03-14 16:50:28 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Michael Green on 3/14/18.
+//  Copyright © 2018 mgreen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
